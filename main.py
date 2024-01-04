3e18a366035d977caae6917f6d77fd87813d3d86
Kostya <pavlenkkovkosta@gmail.com>
2024-01-04 20:37:49 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import random
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from ui import Ui_MainWindow
+from PyQt5 import uic
+
+
+class Example(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), 0))
+        r = random.randint(1, 300)
+        qp.drawEllipse(random.randint(1, 300), random.randint(1, 300), r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
